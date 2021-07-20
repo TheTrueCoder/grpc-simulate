2bf442b132c5d27d5c352c6114e5cf1b04cddcd3
Nathan <38576090+TheTrueCoder@users.noreply.github.com>
2021-07-20 22:49:26 +1200

    Fixed list problem
    Yay for stack overflow!

--- src/basic.py
+++ src/basic.py
@@ -1,36 +1,33 @@
 from random import randint
+from time import sleep
 
-grid_x = 10
-grid_y = 10
+grid_x = 20
+grid_y = 20
 ballno = 10
 minvel = 1
-maxvel = 2
+maxvel = 1
 
 # Functions
 def gen_render_array(balls, x_size, y_size):
-    # render_array = [[' '] * x_size] * y_size
-    # render_array2 = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-    render_array = []
-    for y in range(y_size):
-        for x in range(x_size):
-            render_array[y][x] = " "
-    # if render_array != render_array2:
-    #     print("Oh!")
-    # else:
-    #     print("Nope")
-    print(render_array)
+    # https://stackoverflow.com/questions/240178/list-of-lists-changes-reflected-across-sublists-unexpectedly
+    render_array = [[' ']*x_size for _ in range(y_size)]
     for ball in balls:
         loc = ball["loc"]
-        # print(loc[1], loc[0])
-        render_array[loc[1]][loc[0]] = "o"
-        # print(render_array[loc[1]])
-        # x = render_array[loc[1]]
-        # x[loc[0]] = "o"
-        # print(x)
-        # render_array[loc[1]] = x
-        # print(render_array[1])
-        # render_array[9][1] = "o"
+        try: render_array[loc[1]][loc[0]] = "o"
+        except: pass
     return render_array
+
+# Frame simulate
+def simulate_frame(balls, max_x, max_y):
+    maxes = [max_x, max_y]
+    for ball in balls:
+        for i in range(2):
+            if ball['loc'][i] <= 0 or ball['loc'][i] >= maxes[i]:
+                ball['velocity'][i] *= -1
+                print(ball['velocity'][i])
+            ball['loc'][i] += ball['velocity'][i]
+
+    return balls
 
 # Generate balls
 balls = []
@@ -43,8 +40,9 @@ print(balls)
 
 # Main loop
 while True:
+    print("-"*grid_x*2)
     render_array = gen_render_array(balls, grid_x, grid_y)
-    print(render_array)
     for line in render_array:
         print((" ".join(line)))
-    exit()
+    sleep(0.5)
+    balls = simulate_frame(balls, grid_x, grid_y)
